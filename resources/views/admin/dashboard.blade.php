9ed4f4c35a7b621d2612f952b997afae2dfbef39
Mohammad Shahid <msa.ansari28@gmail.com>
2022-09-19 20:46:47 +0530

    done with AdminMiddleware and Authentication for login

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -7,7 +7,13 @@
         <div class="d-flex justify-content-between flex-wrap">
             <div class="d-flex align-items-end flex-wrap">
                 <div class="me-md-3 me-xl-5">
-                    <h2>Welcome back,</h2>
+                    @if(session('message'))
+                    <!-- <h2>{{ session('message') }}</h2> -->
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('message') }}</strong>
+                        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                    </div>
+                    @endif
                     <p class="mb-md-0">Your analytics dashboard template.</p>
                 </div>
                 <div class="d-flex">
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+//use auth;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -21,7 +23,7 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Route::prefix('admin')->group(function () {
+Route::prefix('admin')->middleware('auth','isAdmin')->group(function () {
 
     Route::get('dashboard', [App\Http\Controllers\Admin\dashboardController::class, 'index']);
 
